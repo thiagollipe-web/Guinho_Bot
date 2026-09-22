e397a2df8de2eafa4420351d9b2c0187ec81c715
thiagollipe-web <thiagollipe@gmail.com>
2026-09-22 18:15:04 -0300

    chore: refresh PWA cache after engine improvements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="guinho-bot-v28";
+const CACHE="guinho-bot-v29";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./knowledge.js","./retrieval.js","./memory.js","./context.js","./probabilistic.js","./prompt-library.js","./game-library.js","./generator.js","./analyzer.js","./fixer.js","./improver.js","./validator.js","./engine.js","./engineering-memory.js","./manifest.json","./icon.svg"];
 const sameOrigin=url=>new URL(url).origin===self.location.origin;
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
