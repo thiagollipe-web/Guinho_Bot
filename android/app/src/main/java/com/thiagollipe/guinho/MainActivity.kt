d39c9b2146bf3cabddf20ce6a2251ac56fc4d2de
thiagollipe-web <thiagollipe@gmail.com>
2026-09-23 19:53:27 -0300

    fix(android): connect GGUF picker bridge to activity

--- android/app/src/main/java/com/thiagollipe/guinho/MainActivity.kt
+++ android/app/src/main/java/com/thiagollipe/guinho/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         NativeLlama.init()
-        nativeAI = GuinhoNativeAI(this, filesDir) { modelId ->
+        nativeAI = GuinhoNativeAI(this, filesDir) { modelId -> selectedModel = modelId; openModel.launch(arrayOf("application/octet-stream", "application/x-gguf", "*/*")) } { modelId ->
             pendingModelId = modelId
             openModel.launch(arrayOf("application/octet-stream", "application/x-gguf", "*/*"))
         }
